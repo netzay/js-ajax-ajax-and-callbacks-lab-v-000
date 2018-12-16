5461d34ca08337ab010916adb8764496b4292336
Ally Netzel <netzelally@gmail.com>
2018-12-16 16:12:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function searchRepositories() {
   $.get(`https://api.github.com/search/repositories?q=${search}`).fail(displayError)
 }
 
-function showCommits() {
+function displayRepositories() {
   let repos = JSON.parse(this.responseText)
   let list = repos.map(r => {
     return `<li><a href=${r.html_url}>${r.name}</a> -
@@ -19,6 +19,12 @@ function showCommits() {
   }).join('')
   let repoList = `<ul>${list}</ul>`
 
+function showCommits(){
+   let show = document.getElementById("searchTerms").value
+
+  $.get(`https://api.github.com/search/repositories?q=${search}`).fail(displayError)
+  
+}
 
 function displayError() {
   $("#errors").html("I'm sorry, there's been an error. Please try again.")
