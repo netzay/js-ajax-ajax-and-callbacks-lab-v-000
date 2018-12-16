6c20067f832ec11348a02e52068a45c3f4711ab0
Ally Netzel <netzelally@gmail.com>
2018-12-16 16:08:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,32 @@
+function searchRepositories() {
+  let search = document.getElementById("searchTerms").value
+
+  $.get(`https://api.github.com/search/repositories?q=${search}`).fail(displayError)
+}
+
+function displayRepositories() {
+  let repos = JSON.parse(this.responseText)
+  let list = repos.map(r => {
+    return `<li><a href=${r.html_url}>${r.name}</a> -
+    <a href='#'
+      data-repository="${r.name}"
+      data-username="${r.owner.login}"
+      onclick='getCommits(this)'> Show Commits </a> -
+    <a href='#'
+      data-repository="${r.name}"
+      data-username="${r.owner.login}"
+      onclick='getBranches(this)'> Show Branches </a></li>`
+  }).join('')
+  let repoList = `<ul>${list}</ul>`
+
+  document.getElementById("repositories").innerHTML = repoList
+}
+
 $(document).ready(function (){
 });
+function showCommits(){
+  
+}
+function displayError() {
+  $("#errors").html("I'm sorry, there's been an error. Please try again.")
+}
