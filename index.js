57febf643fd9e45ef842b718e2a41c937e384214
Ally Netzel <netzelally@gmail.com>
2018-12-16 16:13:41 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,38 +1,58 @@
-function searchRepositories() {
-  let search = document.getElementById("searchTerms").value
-
-  $.get(`https://api.github.com/search/repositories?q=${search}`).fail(displayError)
-}
-
-function displayRepositories() {
-  let repos = JSON.parse(this.responseText)
-  let list = repos.map(r => {
-    return `<li><a href=${r.html_url}>${r.name}</a> -
-    <a href='#'
-      data-repository="${r.name}"
-      data-username="${r.owner.login}"
-      onclick='getCommits(this)'> Show Commits </a> -
-    <a href='#'
-      data-repository="${r.name}"
-      data-username="${r.owner.login}"
-      onclick='getBranches(this)'> Show Branches </a></li>`
-  }).join('')
-  let repoList = `<ul>${list}</ul>`
-
-function showCommits(){
-   let show = document.getElementById("searchTerms").value
-
-  $.get(`https://api.github.com/search/repositories?q=${search}`).fail(displayError)
-  
-}
-
-function displayError() {
-  $("#errors").html("I'm sorry, there's been an error. Please try again.")
-}
-  document.getElementById("repositories").innerHTML = repoList
-}
-
 $(document).ready(function (){
+
 });
+function searchRepositories() {
+  let searchTerms = $("input#searchTerms").val().replace(" ", "+");
+  const requestString = 'https://api.github.com/search/repositories?q=' + searchTerms;
+  $.get(requestString).done(function(data) {
+    console.log(data)
+    const repos = data.items;
+    showRepositories(repos);
+  }).fail(function(error) {
+    displayError(error);
+    console.log('Something went wrong: ' + error);
+  })
+}
 
+function showRepositories(data) {
+  const repoList = '<ul>' + data.map(r => {
+    return (`
+      <li><h2><a href="${r.html_url}">${r.name}</a></h2>
+      <section>
+        <header><h4>Created By ${r.owner.login}</h4></header>
+        <img src="${r.owner.avatar_url}" height="32" width="32">
+      </section>
+      <p>${r.description}</p>
+      <p><a href="#" data-commits_url="${r.commits_url}" data-repository="${r.name}" data-owner="${r.owner.login}" onclick="showCommits(this)">Show Commits</a></p>
+      </li>
+    `)
+  }).join('') + "</ul>"
 
+  $("div#results").html(repoList);
+}
+
+function displayError(error) {
+  $("div#errors").html("${error}");
+}
+
+function showCommits(el) {
+  const requestString = 'https://api.github.com/repos/' + el.dataset.owner + '/' + el.dataset.repository + '/commits';
+  $.get(requestString).done(function(data) {
+    console.log(data)
+    const commitsList = '<ul>' + data.map(c => {
+      return (`
+        <li><h2>${c.sha}</h2>
+        <p>Author: ${c.author.login}</p>
+        <p>Committer Author Name: ${c.commit.author.name}</p>
+        <img src="${c.author.avatar_url}" height="32" width="32">
+        </li>
+      `)
+    }).join('') + "</ul>"
+
+    $("div#details").html(commitsList);
+
+  }).fail(function(error) {
+    displayError(error);
+    console.log('Something went wrong: ' + error);
+  })
+}
